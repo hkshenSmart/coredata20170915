5cc2333aa7d725d8bdf4e59fbfc5971ab3e63a74
hkshen <308421189@qq.com>
2017-09-16 17:13:41 +0800

    Batch.

--- coredata20170915/ViewController.m
+++ coredata20170915/ViewController.m
@@ -15,6 +15,10 @@
 @interface ViewController ()
 
 - (IBAction)doCoreDataCheck:(id)sender;
+- (IBAction)doBatchInsert:(id)sender;
+- (IBAction)doBatchUpdate:(id)sender;
+- (IBAction)doBatchDelete:(id)sender;
+
 
 @end
 
@@ -83,6 +87,102 @@
         // 删
         //[[ClientManage singletonInstance].managedObjectContext deleteObject:checkedPerson];
     }
+    
+    // 保存
+    NSError *error = nil;
+    if ([[ClientManage singletonInstance].managedObjectContext hasChanges] && ![[ClientManage singletonInstance].managedObjectContext save:&error]) {
+        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+        abort();
+    }
+}
+
+- (IBAction)doBatchInsert:(id)sender {
+    
+    for (int i = 0; i < 10; i ++) {
+        
+        Person *person = [NSEntityDescription insertNewObjectForEntityForName:@"Person" inManagedObjectContext:[ClientManage singletonInstance].managedObjectContext];
+        
+        int16_t nameID = arc4random_uniform(9999);
+        person.name = [NSString stringWithFormat:@"name-%d", nameID];
+        
+        [person setValue:[NSNumber numberWithInt:(arc4random_uniform(10) + 10)] forKey:@"age"];
+        
+        // 保存
+        NSError *error = nil;
+        if ([[ClientManage singletonInstance].managedObjectContext hasChanges] && ![[ClientManage singletonInstance].managedObjectContext save:&error]) {
+            NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+            abort();
+        }
+    }
+}
+
+- (IBAction)doBatchUpdate:(id)sender {
+    
+    // 初始化查询请求
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Person" inManagedObjectContext:[ClientManage singletonInstance].managedObjectContext];
+    
+    // 排序方式
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"age" ascending:NO];
+    // 条件过滤
+    //NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == %@", @"hkshen"];
+    
+    fetchRequest.entity = entityDescription;
+    fetchRequest.sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    //fetchRequest.predicate = predicate;
+    
+    NSArray *objectArray = [[ClientManage singletonInstance].managedObjectContext executeFetchRequest:fetchRequest error:nil];
+    
+    if (objectArray) {
+        NSLog(@"objectArray:%@", objectArray);
+        
+        // 1、KVC方式改加载到内存
+        //[objectArray setValue:@"iOS" forKey:@"name"];
+        //NSLog(@"KVC update:%@", objectArray);
+        
+        // 2、NSBatchUpdateRequest不加载到内存，直接对本地数据库进行更新，避免内存不足
+        NSBatchUpdateRequest *batchUpdateRequest = [[NSBatchUpdateRequest alloc] initWithEntityName:@"Person"];
+        //NSPredicate *predicate = [NSPredicate predicateWithFormat:@"age == %@", @(20)];
+        //batchUpdateRequest.predicate = predicate;
+        batchUpdateRequest.propertiesToUpdate = @{@"name": @"hksheniOS"};
+        
+        BOOL isSuccess = batchUpdateRequest.resultType;
+        if (isSuccess) {
+            NSLog(@"isSuccess");
+        }
+        else {
+            NSLog(@"failure");
+        }
+        NSLog(@"NSBatchUpdateRequest update:%@", objectArray);
+    }
+    
+    // 保存
+    NSError *error = nil;
+    if ([[ClientManage singletonInstance].managedObjectContext hasChanges] && ![[ClientManage singletonInstance].managedObjectContext save:&error]) {
+        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+        abort();
+    }
+}
+
+- (IBAction)doBatchDelete:(id)sender {
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Person" inManagedObjectContext:[ClientManage singletonInstance].managedObjectContext];
+    fetchRequest.entity = entityDescription;
+    
+    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"age == %@", @(23)];
+    
+    NSBatchDeleteRequest *batchDeleteRequest = [[NSBatchDeleteRequest alloc] initWithFetchRequest:fetchRequest];
+    batchDeleteRequest.resultType = NSBatchDeleteResultTypeObjectIDs;
+    
+    NSBatchDeleteResult *batchDeleteResult = [[ClientManage singletonInstance].managedObjectContext executeRequest:batchDeleteRequest error:nil];
+    
+    NSArray<NSManagedObjectID *> *batchDeleteResultManagedObjectIDArray = batchDeleteResult.result;
+    
+    // 删除的告诉NSManagedObjectContext
+    NSDictionary *batchDeleteResultDict = @{NSDeletedObjectsKey: batchDeleteResultManagedObjectIDArray};
+    NSManagedObjectContext *managedObjectContex = [ClientManage singletonInstance].managedObjectContext;
+    [NSManagedObjectContext mergeChangesFromRemoteContextSave:batchDeleteResultDict intoContexts:@[managedObjectContex]];
 }
 
 @end
